08919c70e8d56e184c0fa488a57733e4dd5af453
sam ear <sear@sear-mac.local>
2013-02-23 08:05:44 -0800

    first commit

---
+++ git.sh
@@ -0,0 +1,11 @@
+git add foo.js
+git add git.sh
+git add git.txt
+git add list51.js
+git add list52.js
+git add list54.js
+git add node.txt
+git add server.js
+git commit -m "first commit"
+git remote add origin git@github.com:sear/Testing.git
+git push -u origin master
